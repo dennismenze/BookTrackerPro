7de2371a62778aaab20b05077545d7d46f16b148
dorismenze <36667580-dorismenze@users.noreply.replit.com>
2024-09-28 18:28:03 +0000

    Restored to '97d612c2e0ce53bc15ec4e04111303a411d92f71'

--- app.py
+++ app.py
@@ -42,11 +42,6 @@ def create_app():
         books = None
         authors = None
 
-        # Calculate statistics
-        total_books = Book.query.count()
-        total_authors = Author.query.count()
-        total_users = User.query.count()
-
         if current_user.is_authenticated:
             latest_books = Book.query.join(UserBook).filter(
                 UserBook.user_id == current_user.id).order_by(
@@ -81,10 +76,7 @@ def create_app():
                                books=books,
                                book_search_query=book_search_query,
                                authors=authors,
-                               author_search_query=author_search_query,
-                               total_books=total_books,
-                               total_authors=total_authors,
-                               total_users=total_users)
+                               author_search_query=author_search_query)
 
     @app.route('/login', methods=['GET', 'POST'])
     def login():
