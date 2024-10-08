827900b6925540876938bf0297496c4c1ccae146
dorismenze <36667580-dorismenze@users.noreply.replit.com>
2024-10-08 08:57:50 +0000

    Agent step completed: Remove unnecessary jobs from replit.nix file

--- replit.nix
+++ replit.nix
@@ -1,14 +1,7 @@
 {pkgs}: {
   deps = [
-    pkgs.geckodriver
     pkgs.sqlite-interactive
-    pkgs.pg-dump-anon
-    pkgs.rustc
     pkgs.pkg-config
     pkgs.openssl
-    pkgs.libxcrypt
-    pkgs.libiconv
-    pkgs.cargo
-    pkgs.postgresql
   ];
 }
